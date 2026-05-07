429fb861f922b00f05155d74f8724b697b03b3f7
anurupa777 <anurupa777@users.noreply.github.com>
2026-05-07 17:07:06 +0530

    Initial commit

---
+++ day1_basics/Process.js
@@ -0,0 +1,6 @@
+// console.log("Hello World!");
+// console.log("node.js" , process.version)
+// console.log("Process platform: " , process.platform)
+
+let x[]=["Anurupa","Zisna",123]
+console.log(typeof(x))
