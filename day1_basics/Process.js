f2cd0974feeaf61a3356ccddd27d799ae8cc2268
anurupa777 <anurupa777@users.noreply.github.com>
2026-05-07 17:29:12 +0530

    Update process basics example

--- day1_basics/Process.js
+++ day1_basics/Process.js
@@ -2,5 +2,9 @@
 // console.log("node.js" , process.version)
 // console.log("Process platform: " , process.platform)
 
-let x[]=["Anurupa","Zisna",123]
+// let x[]=["Anurupa","Zisna",123]
+// console.log(typeof(x))
+
+
+let x = "Unicorn"
 console.log(typeof(x))
